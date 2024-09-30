fcc42f44a44aa900eefdedae250878be1c194e2d
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2024-09-30 17:00:03 +0200

    TP2 - Nouvelles méthodes pour Village

--- LesGaulois/src/histoire/Scenario.java
+++ LesGaulois/src/histoire/Scenario.java
@@ -1,5 +1,7 @@
 package histoire;
 
+import lieux.Village;
+import personnages.Chef;
 import personnages.Druide;
 import personnages.Gaulois;
 import personnages.Romain;
@@ -7,21 +9,18 @@ import personnages.Romain;
 public class Scenario {
 
 	public static void main(String[] args) {
-		scenario2();	
+		scenario2();
 	}
-	
+
 	public static void scenario1() {
 		/*
-	 	Le gaulois Astérix : « Bonjour à tous»
-		Le romain Minus : « UN GAU... UN GAUGAU...»
-		Astérix envoie un grand coup dans la mâchoire de Minus
-		Le romain Minus : « Aïe»
-		Astérix envoie un grand coup dans la mâchoire de Minus
-		Le romain Minus : « Aïe»
-		Astérix envoie un grand coup dans la mâchoire de Minus
-		Le romain Minus : « J'abandonne...»
-		*/
-		
+		 * Le gaulois Astérix : « Bonjour à tous» Le romain Minus : « UN GAU... UN
+		 * GAUGAU...» Astérix envoie un grand coup dans la mâchoire de Minus Le romain
+		 * Minus : « Aïe» Astérix envoie un grand coup dans la mâchoire de Minus Le
+		 * romain Minus : « Aïe» Astérix envoie un grand coup dans la mâchoire de Minus
+		 * Le romain Minus : « J'abandonne...»
+		 */
+
 		Gaulois asterix = new Gaulois("Astérix", 8);
 		Romain minus = new Romain("Minus", 6);
 
@@ -30,39 +29,37 @@ public class Scenario {
 		asterix.frapper(minus);
 	}
 
-	
 	public static void scenario2() {
 		/*
-		Sachant que minus à une force de 6, astérix une force de 8 et Obélix une force de
-		25, modifier le scénario de la classe Scenario pour obtenir la sortie suivante 
-		
-		Le druide Panoramix : « Bonjour, je suis le druide Panoramix et ma potion peut aller
-		d'une force 5 à 10.»
-		Le druide Panoramix : « Je vais aller préparer une petite potion...»
-		Le druide Panoramix : « Je n'ai pas trouvé tous les ingrédients, ma potion est
-		seulement de force 6.»
-		Le druide Panoramix : « Non, Obélix !... Tu n'auras pas de potion magique !»
-		Le gaulois Obélix : « Par Bélénos, ce n'est pas juste !»
-		Le gaulois Astérix : « Merci Druide, je sens que ma force est 6 fois décuplée.»
-		Le gaulois Astérix : « Bonjour»
-		Le romain Minus : « UN GAU... UN GAUGAU...»
-		Astérix envoie un grand coup dans la mâchoire de Minus
-		*/
-		
+		 * Sachant que minus à une force de 6, astérix une force de 8 et Obélix une
+		 * force de 25, modifier le scénario de la classe Scenario pour obtenir la
+		 * sortie suivante
+		 * 
+		 * Le druide Panoramix : « Bonjour, je suis le druide Panoramix et ma potion
+		 * peut aller d'une force 5 à 10.» Le druide Panoramix : « Je vais aller
+		 * préparer une petite potion...» Le druide Panoramix : « Je n'ai pas trouvé
+		 * tous les ingrédients, ma potion est seulement de force 6.» Le druide
+		 * Panoramix : « Non, Obélix !... Tu n'auras pas de potion magique !» Le gaulois
+		 * Obélix : « Par Bélénos, ce n'est pas juste !» Le gaulois Astérix : « Merci
+		 * Druide, je sens que ma force est 6 fois décuplée.» Le gaulois Astérix : «
+		 * Bonjour» Le romain Minus : « UN GAU... UN GAUGAU...» Astérix envoie un grand
+		 * coup dans la mâchoire de Minus
+		 */
+
 		Romain minus = new Romain("Minus", 6);
 		Gaulois asterix = new Gaulois("Astérix", 8);
 		Gaulois obelix = new Gaulois("Obélix", 25);
 		Druide panoramix = new Druide("Panoramix", 5, 10);
-		
+
 		panoramix.parler("Je vais aller préparer une petite potion...");
 		panoramix.preparerPotion();
-		
+
 		panoramix.booster(obelix);
 		obelix.parler("Par Bélénos, ce n'est pas juste !");
-				
+
 		panoramix.booster(asterix);
 		asterix.parler("Bonjour");
-		
+
 		minus.parler("UN GAU... UN GAUGAU...");
 		asterix.frapper(minus);
 	}
--- LesGaulois/src/lieux/Village.java
+++ LesGaulois/src/lieux/Village.java
@@ -1,13 +1,99 @@
 package lieux;
 
+import java.util.Iterator;
+
+import javax.security.auth.callback.ConfirmationCallback;
+
+import personnages.Chef;
+import personnages.Gaulois;
+
 public class Village {
 	private String nom;
+	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois;
+	private int nbVillageoisMax; 
 	
-	public Village(String nom) {
+	public Village(String nom, int nbVillageoisMax) {
+		this.nbVillageoisMax = nbVillageoisMax;
+		this.villageois = new Gaulois[this.nbVillageoisMax];
 		this.nom = nom;
+	}
+	
+	public void ajouterHabitant(Gaulois gaulois) {
+		this.villageois[nbVillageois] = gaulois;
+		nbVillageois += 1;
+	}
+	
+	public Gaulois trouverHabitant(int place) {
+		return this.villageois[place];
+	}
+	
+	public void afficherVillageois() {
+		String nomChef = "[inconnu]";
+		if (chef != null) {
+			nomChef = chef.getNom();
+		}
+		System.out.println("Dans le village du chef " + nomChef + " vivent les légendaires Gaulois :" );
+		for (int i = 0; i < nbVillageois; i++) {
+			Gaulois gaulois = villageois[i];
+			if (gaulois != null) {
+				System.out.println("- " + gaulois.getNom());				
+			}
+		}
+	}
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+	
+	public Chef getChef() {
+		return chef;
 	}
 	
 	public String getNom() {
 		return nom;
+	}
+	
+	public int getNbVillageois() {
+		return nbVillageois;
+	}
+	
+	public Gaulois[] getVillageois() {
+		return villageois;
+	}
+	
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductibles Gaulois", 30);
+//		village.trouverHabitant(30); 
+		/*
+		 * ^ On obtient cette erreur :
+		 *   Exception in thread "main"
+		 *   java.lang.ArrayIndexOutOfBoundsException: Index 30 out of bounds for length 30 
+		 *  
+		 * C'est parce qu'on a créé un tableau de taille 30, et qu'on tente d'accéder à l'élément d'indice 30, alors que
+		 * l'indice maximal du tableau est 29.
+		 */
+
+		Chef abraracourcix = new Chef("Abraracourcix", 6, village);
+		village.setChef(abraracourcix);
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		village.ajouterHabitant(asterix);
+		Gaulois gaulois = village.trouverHabitant(1);
+		System.out.println(gaulois);
+		/*
+		 * ^ On obtient la sortie suivante :
+		 *     null
+		 * 
+		 * Explication :
+		 * - On crée le village des irréductibles Gaulois
+		 * - On ajoute le chef Abraracourcix
+		 * - On ajoute Astérix en place 0
+		 * - On obtient et affiche le Gaulois en place 0, mais aucun n'a été défini à cet emplacement, on obtient donc null.
+		 */
+
+		Gaulois obelix = new Gaulois("Obélix", 25);
+		village.ajouterHabitant(obelix);
+		village.afficherVillageois();
 	}
 }
